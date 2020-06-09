de2df2d56a29371a83557d3b96b1724518928db2
kamrankhosa <37815528+kamrankhosa@users.noreply.github.com>
2020-06-08 23:54:15 -0700

    Added P_language module and Props into it with parent to child implemetation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Languages from './P_languages';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+        <Languages lang1="C/C++" lang2="Java" lang3="C#" lang4="JavaScript"/>
+     </div>
     </div>
   );
 }
---
+++ src/P_languages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function P_languages(props){
+    return(
+        <div>
+            <h1>Programming languages</h1>
+            <h3>{props.lang1}</h3>
+            <h3>{props.lang2}</h3>
+            <h3>{props.lang3}</h3>
+            <h3>{props.lang4}</h3>
+        </div>
+    )
+}
+export default P_languages;
